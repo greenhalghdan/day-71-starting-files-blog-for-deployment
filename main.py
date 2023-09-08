9b9a5b23c08c9ed6edfd432c5921245fe2ac0ba6
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2023-09-08 18:39:52 +0100

    commit 2

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
+import os
+
 
 
 '''
@@ -26,9 +28,9 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -296,4 +298,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=5001)
